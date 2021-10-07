0c74028304401177f78aa1cf274e8072a7081171
Nele <nuell@cyl.de>
2021-10-07 22:37:37 +0200

    added Task Complete to walking scenes (voice & controller

---
+++ Assets/Scripts/CornerPostsCollision.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CornerPostsCollision : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.name == "[CameraRig]")
+        {
+            print("collision with corner post!");
+            if (transform.tag == "manipulable")
+            {
+                FindObjectOfType<AudioManager>().PlayAudio("SuccessSound");
+                GetComponent<Renderer>().material.color = Color.black;
+                transform.tag = "altered";
+            }
+            
+        }
+    }
+}
---
+++ Assets/Scripts/NavigationSucceded.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class NavigationSucceded : MonoBehaviour
+{
+    GameObject CornerPosts;
+    bool taskCompleted = false;
+    bool notWonalready = true;
+   
+
+    public void Start()
+    {
+        CornerPosts = GameObject.Find("CornerPosts");
+    }
+
+    public void Update()
+    {
+        if (AllCornerPostsAltered())
+        {
+            taskCompleted = true;
+            print("taskCompleted: " + taskCompleted);
+        }
+        
+        if (taskCompleted & notWonalready)
+        {
+            StartCoroutine(TaskCompleted());
+            
+            notWonalready = false;
+        }
+    }
+
+    private IEnumerator TaskCompleted()
+    {
+        yield return new WaitForSeconds(1f);
+        FindObjectOfType<AudioManager>().PlayAudio("WinningSound");
+        
+    }
+
+    bool AllCornerPostsAltered()
+    {
+        if (
+            CornerPosts.transform.GetChild(0).tag == "altered" &
+            CornerPosts.transform.GetChild(1).tag == "altered" &
+            CornerPosts.transform.GetChild(2).tag == "altered" &
+            CornerPosts.transform.GetChild(3).tag == "altered"
+            )
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
